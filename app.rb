44488a02388fb7dbf1758932ea88039961882016
jack-blomquist-nti-johanneberg <jack.blomquist@elev.ga.ntig.se>
2022-01-26 11:31:33 +0100

    grund lagd

--- app.rb
+++ app.rb
@@ -1,1 +1,27 @@
-p "helo"
+
+require 'sinatra'
+require 'slim'
+require 'sqlite3'
+require 'bcrypt'
+
+enable :sessions
+
+get('/') do
+    slim(:index)
+end
+
+get('/register') do
+    slim(:register)
+end
+
+get('/login') do
+    slim(:login)
+end
+
+get('/users/index') do
+    slim(:"users/index")
+end
+
+post('/users/new') do
+    redirect('/users/index')
+end
