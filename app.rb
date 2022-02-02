94c850f1731bafc76fefced29f156ae3a06d2ac9
jack-blomquist-nti-johanneberg <jack.blomquist@elev.ga.ntig.se>
2022-02-02 11:40:47 +0100

    fixa register

--- app.rb
+++ app.rb
@@ -6,6 +6,26 @@ require 'bcrypt'
 
 enable :sessions
 
+# setup
+
+clear_message_routes = ["/register"]
+
+def db_connection(route)
+    db = SQLite3::Database.new(route)
+    db.results_as_hash = true
+    return db
+end
+
+before do
+    if session[:message] != nil && not(clear_message_routes.include?(request.path_info))
+        p request.path_info
+        p "hello"
+        session[:message] = nil
+    end
+end
+
+#get routes
+
 get('/') do
     slim(:index)
 end
@@ -18,10 +38,24 @@ get('/login') do
     slim(:login)
 end
 
-get('/users/index') do
+get('/users/:id/profile') do
     slim(:"users/index")
 end
 
+#post routes
+
 post('/users/new') do
-    redirect('/users/index')
+    db = db_connection('db/db.db')
+    username = params[:username]
+    password = params[:password]
+    ver_password = params[:ver_password]
+
+    if password == ver_password
+        salted_password = password + "salt"
+        crypted_password = BCrypt::Password.create(salted_password)
+        db.execute("INSERT INTO users(username,password,role) VALUES (?,?,?)",username,crypted_password,"guest")
+    else
+        session[:message] = "Register failed: password not equal to ver_password"
+    end
+    redirect('/register')
 end
