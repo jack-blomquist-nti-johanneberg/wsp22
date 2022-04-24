ec4a493ed2898c264a32fa2c7da3ce853d27aee9
jack-blomquist-nti-johanneberg <jack.blomquist@elev.ga.ntig.se>
2022-04-24 18:16:09 +0200

    projekt avklarat

--- app.rb
+++ app.rb
@@ -7,12 +7,10 @@ require_relative './model.rb'
 
 enable :sessions
 
-# setup
-
 clear_message_routes = false
 
-#clear session-cookies
-
+# Clears the message cookie every other re-route
+#
 before do
     if session[:message]
         if clear_message_routes
@@ -24,37 +22,53 @@ before do
     end
 end
 
-#get routes
-
+# Displays Landing Page
+#
+# @see Model#ghost_users
+# @see Model#get_recipes
 get('/') do
     keyword = params[:search]
 
     slim(:index, locals:{recipes:ghost_users(get_recipes(keyword)[0]),genres:get_recipes(keyword)[1]})
 end
 
+# Displays Error Page
+#
 get('/error') do
     slim(:error)
 end
 
+# Displays Register Page
+#
+# @register_texts [Array] texts, Texts that can appear when routing to the Register page
 get('/register') do
     @register_texts = [" make tasty pancakes!"," make bread!"," live in harmony!","... um... cook rice?"," impress family and friends!", " avoid unforeseen consequences...!"]
     slim(:"users/new")
 end
 
+# Displays Login Page
+#
 get('/login') do
     slim(:login)
 end
 
+# Displays a User Page
+#
+# @param [Integer] user_id, the ID of the user who's page you are on
+#
+# @see Model#get_user
 get('/users/:id') do
-    user_id = params[:id]
+    user_id = params[:id].to_i
 
     slim(:"users/index", locals:{users_info:get_user(user_id)[0],users_recipes:get_user(user_id)[1]})
 end
 
+# Displays the Edit Page for a user
+#
+# @param [Integer] user_id, the ID of your user
 get('/users/:id/edit') do
-    user_id = params[:id]
-    if user_id.to_i == session[:active_user_id].to_i
-
+    user_id = params[:id].to_i
+    if user_id == session[:active_user_id]
         slim(:"users/edit")
     else
         session[:message] = "Re-routing failed: you cannot edit someone elses profile!"
@@ -62,6 +76,8 @@ get('/users/:id/edit') do
     end
 end
 
+# Displays the Recipe Page for making new recipes
+#
 get('/recipes/new') do
     if session[:active_user_role] != "guest" && session[:active_user_role] != nil
         slim(:"recipes/new")
@@ -71,33 +87,59 @@ get('/recipes/new') do
     end
 end
 
+# Displays a Recipe Page
+#
+# @param [Integer] recipe_id, the ID for the recipe
+#
+# @see Model#get_comments
+# @see Model#get_recipe_data
 get('/recipes/:id') do
-    recipe_id = params[:id]
+    recipe_id = params[:id].to_i
     session[:recipe_id] = recipe_id
 
     get_comments(recipe_id)
 
+    if get_recipe_data(recipe_id).nil?
+        session[:message] = "Re-routing failed: recipe does not exist"
+        redirect('/error')
+    end
+
     slim(:"recipes/index", locals:{recipes_info:get_recipe_data(recipe_id)})
 end
 
+# Displays the Recipe Edit Page
+#
+# @param [Integer] recipe_id, the ID for the recipe
+#
+# @see Model#owner_check
 get('/recipes/:id/edit') do
     recipe_id = params[:id]
 
-    owner_check(recipe_id)['user_id']
-
-    if owner_check(recipe_id)['user_id'] == session[:active_user_id] || session[:active_user_role] == "admin"
-        @recipe_name = owner_check(recipe_id)['title']
-        slim(:"recipes/edit",locals:{recipe_id:recipe_id})
-    else
-        session[:message] = "Re-routing failed: you have to be either be an admin or the owner to edit this recipe"
+    if owner_check(recipe_id).class == NilClass
+        session[:message] = "Re-routing failed: recipe does not exist"
         redirect('/error')
+    else
+        if owner_check(recipe_id)['user_id'] == session[:active_user_id] || session[:active_user_role] == "admin"
+            @recipe_name = owner_check(recipe_id)['title']
+            slim(:"recipes/edit",locals:{recipe_id:recipe_id})
+        else
+            session[:message] = "Re-routing failed: you have to be either be an admin or the owner to edit this recipe"
+            redirect('/error')
+        end
     end
 end
 
-#post routes
-
+# Creates a new user and redirects to '/register'
+#
+# @param [String] username, the username for the new user
+# @param [String] password, the password for the new user
+# @param [String] ver_password, the verification of the password
+#
+# @see Model#length_check
+# @see Model#username_check
+# @see Model#time_check
+# @see Model#user_creation
 post('/users') do
-    db = db_connection('db/db.db')
     username = params[:username]
     password = params[:password]
     ver_password = params[:ver_password]
@@ -107,113 +149,132 @@ post('/users') do
         redirect('/register')
     end
 
-    if db.execute("SELECT username FROM users WHERE username=(?)",username).first != nil
+    if username_check(username) != nil
         session[:message] = "Register failed: name already taken!"
         redirect('/register')
     end
 
     $stress_array << Time.now.to_i
-    p $stress_array
 
     if time_check($stress_array,6)
         session[:message] = "Register failed: too much pressure"
         redirect('/register')
     end
 
-    if password == ver_password
-        salted_password = password + "salt"
-        crypted_password = BCrypt::Password.create(salted_password)
-        db.execute("INSERT INTO users(username,password,role) VALUES (?,?,?)",username,crypted_password,"guest")
+    if user_creation(username,password,ver_password)
         session[:message] = "User created!"
         redirect('/login')
     else
-        session[:message] = "Register failed: password not equal to ver_password"
+        session[:message] = "Register failed: password not equal to verify password"
         redirect('/register')
     end
 end
 
+# Logins the user and updates sessions and redirects 'login' or '/'
+#
+# @param [String] username, the username for the user
+# @param [String] password, the password for the user
+#
+# @see Model#login_check
 post('/login') do
-    db = db_connection('db/db.db')
     username = params[:username]
     password = params[:password]
 
-    login_check = db.execute("SELECT * FROM users WHERE username=(?)",username).first
-
-    if login_check != nil
-        if BCrypt::Password.new(login_check["password"]) == (password + "salt")
-            update_active_user(login_check['username'],login_check['id'],login_check['role'])
-            redirect('/')
-        else
-            session[:message] = "Login failed: invalid input"
-            redirect('/login')
-        end
+    if login_check(username,password) == true
+        redirect('/')
     else
-        session[:message] = "Login failed: user does not exist"
+        session[:message] = login_check(username,password)[-1]
         redirect('/login')
     end
 end
 
+# Logouts the user and clears sessions and redirects to '/'
+#
+# @see Model#update_active_user
 post('/logout') do
     update_active_user(nil,nil,nil)
 
     redirect('/')
 end
 
+# Updates a user and redirects either to "/users/#{user_id}/edit" or "/users/#{user_id}"
+#
+# @param [Integer] user_id, the ID of the user being edited
+# @param [String] email, the email of the user
+# @param [String] new_username, the new username
+#
+# @see Model#email_check
+# @see Model#update_active_user
+# @see Model#change_username
 post('/users/:id/update') do
-    db = db_connection('db/db.db')
-    user_id = params[:id]
+    user_id = params[:id].to_i
     email = params[:email]
     new_username = params[:username]
 
     if email != nil
-        if email.include?("@")
-            db.execute("UPDATE users SET role='verified' WHERE id=(?)",user_id)
-            db.execute("UPDATE users SET email=(?) WHERE id=(?)",email,user_id)
-            session[:active_user_role] = db.execute("SELECT role FROM users WHERE id=(?)",user_id).first
-
-            redirect("/users/#{user_id}")
-        else
+        if not(email_check(email,user_id))
             session[:message] = "User update failed: you must enter an actual email adress"
-
             redirect("/users/#{user_id}/edit")
+        else
+            update_active_user(session[:active_user],session[:active_user_id],email_check(email,user_id)[-1])
+            redirect("/users/#{user_id}")
         end
     end
 
-    if length_check(new_username,33)
+    if change_username(new_username,user_id)
+        session[:active_user] = new_username
+        redirect("/users/#{user_id}")
+    else
         session[:message] = "User update failed: new username too long, it must be shorter than 33 characters"
         redirect("/users/#{user_id}/edit")
-    else
-        db.execute("UPDATE users SET username=(?) WHERE id=(?)",new_username,user_id)
-        session[:active_user] = new_username
-
-        redirect("/users/#{user_id}")
     end
 end
 
+# Deletes the logged in user and redirects to '/'
+#
+# @param [Integer] id, the ID of the user
+#
+# @see Model#delete_user
+# @see Model#update_active_user
 post('/users/:id/delete') do
-    db = db_connection('db/db.db')
     id = params[:id].to_i
 
-    db.execute("DELETE FROM users WHERE id=(?)",id)
+    delete_user(id)
     update_active_user(nil,nil,nil)
 
     redirect('/')
 end
 
+# Creates a new comment and redirects to "/recipes/#{recipe_id}"
+#
+# @param [String] comment, the content of the comment
+# @param [Integer] recipe_id, the ID of the recipe being commented on
+#
+# @see Model#post_comment
 post('/comment') do
-    db = db_connection('db/db.db')
     comment = params[:comment]
     recipe_id = session[:recipe_id].to_i
-
     date = Time.now.strftime("%Y-%b-%d")
 
-    db.execute("INSERT INTO comments(user_id,recipe_id,content,date) VALUES (?,?,?,?)",session[:active_user_id].to_i,recipe_id,comment,date)
+    post_comment(recipe_id,comment,date)
 
     redirect("/recipes/#{recipe_id}")
 end
 
+# Creates a new recipe and redirects to "/users/#{session[:active_user_id]}"
+#
+# @param [String] title, the title of the recipe
+# @param [String] background, the background of the recipe
+# @param [String] ingredients, the ingredients of the recipe
+# @param [String] steps, the steps of the recipe
+# @param [String] genre1, the first genre of the recipe
+# @param [String] genre2, the second genre of the recipe
+# @param [String] genre3, the third genre of the recipe
+#
+# @see Model#length_check
+# @see Model#time_check
+# @see Model#post_recipe
 post('/recipes') do
-    db = db_connection('db/db.db')
     title = params[:title].to_s
     background = params[:background].to_s
     ingredients = params[:ingredients].to_s
@@ -228,28 +289,30 @@ post('/recipes') do
     end
 
     $stress_array << Time.now.to_i
-    p $stress_array
 
     if time_check($stress_array,20)
         session[:message] = "New recipe failed: too much pressure"
         redirect('/recipes/new')
     end
 
-    db.execute("INSERT INTO recipes(title,info,ingredients,steps,user_id) VALUES (?,?,?,?,?)",title,background,ingredients,steps,session[:active_user_id])
-
-    latest_recipe = db.execute("SELECT id FROM recipes ORDER BY id DESC").first
-
-    genres_id = db.execute("SELECT id FROM genres WHERE genre IN (?,?,?)",genre1,genre2,genre3)
-
-    genres_id.each do |genre|
-        db.execute("INSERT INTO recipes_genre_rel(recipe_id,genre_id) VALUES (?,?)",latest_recipe['id'],genre['id'])
-    end
+    post_recipe(title,background,ingredients,steps,genre1,genre2,genre3)
 
     redirect("/users/#{session[:active_user_id]}")
 end
 
+# Updates a recipe and redirects to '/'
+#
+# @param [Integer] id, the id of the recipe
+# @param [String] title, the title of the recipe
+# @param [String] background, the background of the recipe
+# @param [String] ingredients, the ingredients of the recipe
+# @param [String] steps, the steps of the recipe
+# @param [String] genre1, the first genre of the recipe
+# @param [String] genre2, the second genre of the recipe
+# @param [String] genre3, the third genre of the recipe
+#
+# @see Model#change_recipe
 post('/recipes/:id/update') do
-    db = db_connection('db/db.db')
     id = params[:id].to_i
     title = params[:title].to_s
     background = params[:background].to_s
@@ -259,25 +322,20 @@ post('/recipes/:id/update') do
     genre2 = params[:genre2].to_s
     genre3 = params[:genre3].to_s
 
-    db.execute("UPDATE recipes SET title=(?),info=(?),ingredients=(?),steps=(?) WHERE id=(?)",title,background,ingredients,steps,id)
-
-    genres_id = db.execute("SELECT id FROM genres WHERE genre IN (?,?,?)",genre1,genre2,genre3)
-
-    rel_id = db.execute("SELECT id FROM recipes_genre_rel WHERE recipe_id=(?)",id)
-
-    genres_id.each_with_index do |genre,index|
-        db.execute("UPDATE recipes_genre_rel SET genre_id=(?) WHERE recipe_id=(?) AND id=(?)",genre['id'],id,rel_id[index]['id'])
-    end
+    change_recipe(id,title,background,ingredients,steps,genre1,genre2,genre3)
 
     redirect('/')
 end
 
-post ('/recipes/:id/delete') do
-    db = db_connection('db/db.db')
+# Deletes a recipe and redirects to '/'
+#
+# @param [Integer] id, the id of the recipe
+#
+# @see Model#delete_recipe
+post('/recipes/:id/delete') do
     id = params[:id].to_i
 
-    db.execute("DELETE FROM recipes WHERE id=(?)",id)
-    db.execute("DELETE FROM recipes_genre_rel WHERE recipe_id=(?)",id)
+    delete_recipe(id)
 
     redirect('/')
 end
--- model.rb
+++ model.rb
@@ -1,20 +1,37 @@
 
-# Setup
 $stress_array = []
 
+# Establishes a connection to the database
+#
+# @param [String] route
+#
+# @return [SQLite3::Database] allowing communications with the database
 def db_connection(route)
     db = SQLite3::Database.new(route)
     db.results_as_hash = true
+    p db.class
     return db
 end
 
+# Updates sessions regarding user details
+#
+# @param [String] name
+# @param [Integer] id
+# @param [String] role
+#
+# @return [Nil]
 def update_active_user(name,id,role)
     session[:active_user] = name
-    session[:active_user_id] = id
+    session[:active_user_id] = id.to_i
     session[:active_user_role] = role
     return nil
 end
 
+# Updates information about visible users to decide if they are deleted or not
+#
+# @param [String] array
+#
+# @return [Array] with some elements being replaced if users have been deleted
 def ghost_users(array)
     db = db_connection('db/db.db')
     array.each do |ary|
@@ -31,6 +48,12 @@ def ghost_users(array)
     return array
 end
 
+# Checks the length of a string
+#
+# @param [String] string
+# @param [Integer] len
+#
+# @return [Boolean] based on if the inputed string was too long or not
 def length_check(string,len)
     if string.length >= len
         return true
@@ -39,6 +62,12 @@ def length_check(string,len)
     end
 end
 
+# Checks if the time between two events is too great
+#
+# @param [Array] time_array
+# @param [String] time
+#
+# @return [Boolean] based on if the taken time was too long or not
 def time_check(time_array, time)
     if time_array.length == 2
         $stress_array = []
@@ -52,12 +81,79 @@ def time_check(time_array, time)
     end
 end
 
-# Get metoder
+# Checks if recipe exists and gives information if it does
+#
+# @param [Integer] id
+#
+# @return [Hash] 
+#   * :user_id [Integer] the id of the user
+#   * :title [String] the title of the recipe
+def owner_check(id)
+    db = db_connection('db/db.db')
+    return db.execute("SELECT user_id,title FROM recipes WHERE id=(?)",id).first
+end
+
+# Checks if username is avaliable and gives information if it does
+#
+# @param [String] username
+#
+# @return [Hash] 
+#   * :username [String] the username
+def username_check(username)
+    db = db_connection('db/db.db')
+    return db.execute("SELECT username FROM users WHERE username=(?)",username).first
+end
+
+# Logins the user if the password is correct
+#
+# @param [String] username
+# @param [String] password
+#
+# @return [Array] containing both a boolean and an error message 
+def login_check(username,password)
+    db = db_connection('db/db.db')
+    login_check = db.execute("SELECT * FROM users WHERE username=(?)",username).first
+
+    if login_check != nil
+        if BCrypt::Password.new(login_check["password"]) == (password + "salt")
+            update_active_user(login_check['username'],login_check['id'],login_check['role'])
+            return true
+        else
+            return false,"Login failed: wrong password"
+        end
+    else
+        return false,"Login failed: user does not exist"
+    end
+end
+
+# Checks if the email is in a correct format
+#
+# @param [String] email
+# @param [Integer] id
+#
+# @return [Array] containing both a boolean and the updated role if true
+def email_check(email,id)
+    db = db_connection('db/db.db')
+
+    if email.include?("@")
+        db.execute("UPDATE users SET role='verified' WHERE id=(?)",id)
+        db.execute("UPDATE users SET email=(?) WHERE id=(?)",email,id)
+        return true,db.execute("SELECT role FROM users WHERE id=(?)",id).first
+    else
+        return false
+    end
+end
+
+# Selects the recipes based on a keyword
+#
+# @param [String] keyword
+#
+# @return [Array] containing both the recipe information and genres for the recipes
 def get_recipes(keyword)
     db = db_connection('db/db.db')
     genres = []
 
-    if keyword == ""
+    if keyword == "" || keyword == nil
         recipes = db.execute("SELECT id,title,user_id FROM recipes")
     else
         recipes = db.execute("SELECT id,title,user_id FROM recipes WHERE title=(?)",keyword)
@@ -70,6 +166,11 @@ def get_recipes(keyword)
     return recipes,genres
 end
 
+# Finds the user page
+#
+# @param [Integer] id
+#
+# @return [Array] containing both user data and user recipes
 def get_user(id)
     db = db_connection('db/db.db')
     user_data = db.execute("SELECT * FROM users WHERE id=(?)",id).first
@@ -83,30 +184,170 @@ def get_user(id)
     return user_data,user_recipes
 end
 
+# Retrieves data about a recipe
+#
+# @param [Integer] id
+#
+# @return [Hash]
+#   * :id [Integer] the id of the recipe
+#   * :title [String] the title of the recipe
+#   * :info [String] the background of the recipe
+#   * :ingredients [String] the ingredients of the recipe
+#   * :steps [String] the steps of the recipe
+#   * :user_id [Integer] the id of the recipe
 def get_recipe_data(id)
     db = db_connection('db/db.db')
     recipe_data = db.execute("SELECT * FROM recipes WHERE id=(?)",id)
-
-    if recipe_data.nil?
-        session[:message] = "Re-routing failed: recipe does not exist"
-        redirect('/error')
-    end
 
     ghost_users(recipe_data)
 
     return recipe_data.first
 end
 
+# Retrieves the comments
+#
+# @param [Integer] id
+#
+# @return [Nil]
 def get_comments(id)
     db = db_connection('db/db.db')
     @comments = db.execute("SELECT content,date,user_id FROM comments WHERE recipe_id=(?)",id)
 
     ghost_users(@comments)
+
+    return nil
 end
 
-def owner_check(id)
+# Creates a new user
+#
+# @param [String] username
+# @param [String] password
+# @param [String] ver_password
+#
+# @return [Boolean] based on password was equal to ver_password
+def user_creation(username,password,ver_password)
     db = db_connection('db/db.db')
-    return db.execute("SELECT user_id,title FROM recipes WHERE id=(?)",id).first
+
+    if password == ver_password
+        salted_password = password + "salt"
+        crypted_password = BCrypt::Password.create(salted_password)
+        db.execute("INSERT INTO users(username,password,role) VALUES (?,?,?)",username,crypted_password,"guest")
+        return true
+    else
+        return false
+    end
 end
 
-# Post metoder
+# Changes the username of a user
+#
+# @param [String] new_username
+# @param [Integer] id
+#
+# @return [Boolean] since the username can be too long
+def change_username(new_username,id)
+    db = db_connection('db/db.db')
+
+    if length_check(new_username,33)
+        return false
+    else
+        db.execute("UPDATE users SET username=(?) WHERE id=(?)",new_username,id)
+        return true
+    end
+end
+
+# Deletes a user
+#
+# @param [Integer] id
+#
+# @return [Nil]
+def delete_user(id)
+    db = db_connection('db/db.db')
+
+    db.execute("DELETE FROM users WHERE id=(?)",id)
+
+    return nil
+end
+
+# Creates a new comment
+#
+# @param [Integer] recipe_id
+# @param [String] comment
+# @param [String] date
+#
+# @return [Nil]
+def post_comment(recipe_id,comment,date)
+    db = db_connection('db/db.db')
+
+    db.execute("INSERT INTO comments(user_id,recipe_id,content,date) VALUES (?,?,?,?)",session[:active_user_id].to_i,recipe_id,comment,date)
+
+    return nil
+end
+
+# Creates a new recipe
+#
+# @param [String] title
+# @param [String] background
+# @param [String] ingredients
+# @param [String] steps
+# @param [String] genre1
+# @param [String] genre2
+# @param [String] genre3
+#
+# @return [Nil]
+def post_recipe(title,background,ingredients,steps,genre1,genre2,genre3)
+    db = db_connection('db/db.db')
+
+    db.execute("INSERT INTO recipes(title,info,ingredients,steps,user_id) VALUES (?,?,?,?,?)",title,background,ingredients,steps,session[:active_user_id].to_i)
+
+    latest_recipe = db.execute("SELECT id FROM recipes ORDER BY id DESC").first
+
+    genres_id = db.execute("SELECT id FROM genres WHERE genre IN (?,?,?)",genre1,genre2,genre3)
+
+    genres_id.each do |genre|
+        db.execute("INSERT INTO recipes_genre_rel(recipe_id,genre_id) VALUES (?,?)",latest_recipe['id'],genre['id'])
+    end
+
+    return nil
+end
+
+# Updates a recipe
+#
+# @param [Integer] id
+# @param [String] title
+# @param [String] background
+# @param [String] ingredients
+# @param [String] steps
+# @param [String] genre1
+# @param [String] genre2
+# @param [String] genre3
+#
+# @return [Nil]
+def change_recipe(id,title,background,ingredients,steps,genre1,genre2,genre3)
+    db = db_connection('db/db.db')
+
+    db.execute("UPDATE recipes SET title=(?),info=(?),ingredients=(?),steps=(?) WHERE id=(?)",title,background,ingredients,steps,id)
+
+    genres_id = db.execute("SELECT id FROM genres WHERE genre IN (?,?,?)",genre1,genre2,genre3)
+
+    rel_id = db.execute("SELECT id FROM recipes_genre_rel WHERE recipe_id=(?)",id)
+
+    genres_id.each_with_index do |genre,index|
+        db.execute("UPDATE recipes_genre_rel SET genre_id=(?) WHERE recipe_id=(?) AND id=(?)",genre['id'],id,rel_id[index]['id'])
+    end
+
+    return nil
+end
+
+# Deletes a recipe
+#
+# @param [Integer] id
+#
+# @return [Nil]
+def delete_recipe(id)
+    db = db_connection('db/db.db')
+
+    db.execute("DELETE FROM recipes WHERE id=(?)",id)
+    db.execute("DELETE FROM recipes_genre_rel WHERE recipe_id=(?)",id)
+
+    return nil
+end
